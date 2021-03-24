5e7e4c3876e9f87751f9e2781eb0433b924fbd2f
Peter Brännström <54285305+Brannstroom@users.noreply.github.com>
2021-03-24 23:37:41 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,14 +8,21 @@ bot = commands.Bot(command_prefix = '!')
 async def on_ready():
     print("Hei botten har starta bro")
 
-@bot.command()
-async def test(ctx, arg):
-    max = random.randint(1,int(arg))
-    await ctx.send("Du fikk " + str(max))
-
 @bot.command(aliases=['vs'])
 async def versus(ctx):
+    author = ctx.message.author
 
+    embed=discord.Embed(title="Versus", color=0x0e87f1)
+    embed.add_field(name="Settings", value="1️⃣ - Antall lag: 2 \n ❌ - Lås kanalen for motstandere: NEI \n 👪 - Inkluder alle på servern: NEI ", inline=False)
+    embed.add_field(name="Spillere", value="1234 \n 1234 \n 1234 \n 1234 \n 1234 \n 1234", inline=True)
+    message = await ctx.send(embed=embed)
+
+    emojis = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '❌', '👪']
+    for emoji in emojis:
+        await message.add_reaction(emoji)
+
+@bot.event
+async def on_reaction_add():
     
 
 
