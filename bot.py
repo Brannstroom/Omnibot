c70664352baa4f56843c093c160ce0a0918c5ffb
Peter Brännström <54285305+Brannstroom@users.noreply.github.com>
2021-03-25 00:54:40 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,19 +11,23 @@ async def on_ready():
 @bot.command(aliases=['vs'])
 async def versus(ctx):
     author = ctx.message.author
+    channel = author.voice.channel
+
+    members_in_vc = ""
+
+    for member in channel.members:
+        members_in_vc += member.mention + '\n'
+
+    tms = str(members_in_vc)
 
     embed=discord.Embed(title="Versus", color=0x0e87f1)
-    embed.add_field(name="Settings", value="1️⃣ - Antall lag: 2 \n ❌ - Lås kanalen for motstandere: NEI \n 👪 - Inkluder alle på servern: NEI ", inline=False)
-    embed.add_field(name="Spillere", value="1234 \n 1234 \n 1234 \n 1234 \n 1234 \n 1234", inline=True)
+    embed.add_field(name="Settings", value="1️⃣ - Antall lag \n\n ❌ - Lås kanalen for motstandere \n\n 👪 - Inkluder alle på servern", inline=True)
+    embed.add_field(name="Status", value="2 \n\n NEI \n\n NEI", inline=True)
+    embed.add_field(name="Spillere", value=tms, inline=False)
     message = await ctx.send(embed=embed)
 
     emojis = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '❌', '👪']
     for emoji in emojis:
         await message.add_reaction(emoji)
-
-@bot.event
-async def on_reaction_add():
-    
-
 
 bot.run('ODI0MzQ3NDkyMjQxODk5NTUy.YFuDbQ.O7k6dU3U5w7nU-FpTqD8n-xrVKU')
