53948e9716be7ea7a0e9580ddb30d41751f4bc4d
Peter Brännström <54285305+Brannstroom@users.noreply.github.com>
2021-03-24 20:32:18 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,14 @@
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '')
+client = commands.Bot(command_prefix = '!')
+
+@client.event
+async def on_ready():
+    print("Hei botten har starta bro")
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong!')
+
+client.run('ODI0MzQ3NDkyMjQxODk5NTUy.YFuDbQ.O7k6dU3U5w7nU-FpTqD8n-xrVKU')
